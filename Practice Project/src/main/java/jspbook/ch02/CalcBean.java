ad393c7c7608e8fbdc76f90455cc4119fcf3bd7e
khsexk <kohyunsuk98@gmail.com>
2021-10-26 09:42:43 +0900

    JSP 시험 공부

--- Practice Project/src/main/java/jspbook/ch02/CalcBean.java
+++ Practice Project/src/main/java/jspbook/ch02/CalcBean.java
@@ -12,33 +12,17 @@ public class CalcBean {
       else if(operator.equals("/")){result = num1 / num2;}
    }
 
-
-   public void setNum1(int num1) {
-      this.num1 = num1;
-   }
+   public void setNum1(int num1) { this.num1 = num1; }
    
-   public void setNum2(int num2) {
-      this.num2 = num2;
-   }
+   public void setNum2(int num2) { this.num2 = num2; }
 
-   public int getNum1() {
-      return  num1;
-   }
+   public int getNum1() { return  num1; }
    
-   public int getNum2() {
-      return num2;
-   }
+   public int getNum2() { return num2; }
 
-   public String getOperator() {
-      return operator;
-   }
+   public String getOperator() { return operator; }
 
-   public void setOperator(String operator) {
-      this.operator = operator;
-   }
+   public void setOperator(String operator) { this.operator = operator; }
 
-   public int getResult() {
-      return result;
-   }
-
+   public int getResult() { return result; }
 }
